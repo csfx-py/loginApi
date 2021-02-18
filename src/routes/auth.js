d45ba0ae396a5c00616ec22eb7533a6a5fb38d89
csfx-py <csfxshreyas@gmail.com>
2021-02-18 23:49:27 +0530

    expiry date testing

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -57,7 +57,7 @@ router.post("/login", async (req, res) => {
 
   // create token
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SEC, {
-    expiresIn: Math.floor((new Date(user.date).getTime() + 31556952000)/1000),
+    expiresIn: 10000,
   });
   res
     .header("auth-token", token)
