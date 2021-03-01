edac468d1f5f984ae7ba71156d58a3024174d444
csfx-py <csfxshreyas@gmail.com>
2021-03-01 14:05:43 +0530

    added promise resolve to db connect

--- src/index.js
+++ src/index.js
@@ -12,10 +12,12 @@ const postRoute = require("./routes/page");
 const app = express();
 app.use(express.json());
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB"));
 
 app.use("/user", authRoute);
 app.use("/posts", postRoute);
